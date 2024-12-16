d72792203b1fd0cd2b7a075fa960f93b9964ea6b
chenzihong-gavin <chenzihong_gavin@foxmail.com>
2024-12-16 17:28:26 +0800

    refactor(utils): refact utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,3 +4,4 @@ from .format import (pack_history_conversations, split_string_by_multi_markers,
                      handle_single_entity_extraction, handle_single_relationship_extraction,
                      load_json, write_json)
 from .hash import compute_content_hash, compute_args_hash
+from .chunk import chunk_by_token_size

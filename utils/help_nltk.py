c242188db1ee7cd5b3ceca3c780850a3ccbd1728
chenzihong-gavin <chenzihong_gavin@foxmail.com>
2024-12-24 15:47:54 +0800

    feat(utils): word tokenize using nltk & jieba

--- utils/help_nltk.py
+++ utils/help_nltk.py
@@ -1,14 +1,19 @@
 import os
 import nltk
+import jieba
 from typing import Dict, List, Optional
 
 resource_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "resources")
+
 
 class NLTKHelper:
     _stopwords: Dict[str, Optional[List[str]]] = {
         "english": None,
         "chinese": None,
     }
+
+    def __init__(self):
+        jieba.initialize()
 
     def get_stopwords(self, lang: str) -> List[str]:
         nltk.data.path.append(os.path.join(resource_path, "nltk_data"))
@@ -20,3 +25,16 @@ class NLTKHelper:
 
             self._stopwords[lang] = nltk.corpus.stopwords.words(lang)
         return self._stopwords[lang]
+
+    @staticmethod
+    def word_tokenize(text: str, lang: str) -> List[str]:
+        if lang == "chinese":
+            return jieba.lcut(text)
+        else:
+            nltk.data.path.append(os.path.join(resource_path, "nltk_data"))
+            try:
+                nltk.data.find("tokenizers/punkt_tab")
+            except LookupError:
+                nltk.download("punkt_tab", download_dir=os.path.join(resource_path, "nltk_data"))
+
+            return nltk.word_tokenize(text, language=lang)

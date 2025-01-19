f8450100da1101281c0862b7774876c405f9f204
chenzihong-gavin <chenzihong_gavin@foxmail.com>
2025-01-19 20:26:06 +0800

    feat(charts): add plot_metric_trend method

--- charts/plot_metric_trend.py
+++ charts/plot_metric_trend.py
@@ -1,58 +1,79 @@
 import pandas as pd
 import plotly.express as px
+import numpy as np
+from scipy.interpolate import make_interp_spline
 
 def plot_metric_trend(df, x, y):
-    fig = px.line(df, x=x, y=y, color='epoch', markers=True)
+
+    fig = px.line(df, x=x, y=y, color='max length', markers=True, color_discrete_sequence=['#925EB0', '#7E99F4', '#CC7C71', '#7AB656']) # A5AEB7
 
     fig.update_xaxes(tickvals=df[x], ticktext=[f'{int(val * 100)}%' for val in df[x].unique()])
 
+    avg = df.groupby(x)[y].mean().reset_index()
+    avg['max length'] = 'Average'
+
+    x_smooth = np.linspace(avg[x].min(), avg[x].max(), 500)
+    spline = make_interp_spline(avg[x], avg[y], k=3)  # k=3 for cubic spline
+    y_smooth = spline(x_smooth)
+
+    # Add the smooth average line to the plot
+    fig.add_scatter(x=x_smooth, y=y_smooth, mode='lines', name='Smooth Average',
+                    line={
+                        'color': 'red',
+                        'width': 4,
+                        'dash': 'solid'
+                    })
+
     fig.update_layout(xaxis_title='Percentage',
-                      yaxis_title='Agr Average Improvement',
-                      plot_bgcolor='rgba(0,0,0,0)',  # 设置绘图区域背景为透明
-                      paper_bgcolor='rgba(0,0,0,0)',  # 设置整个图表背景为透明
+                      yaxis_title='Agr Average',
+                      plot_bgcolor='rgba(0,0,0,0)',
+                      paper_bgcolor='rgba(0,0,0,0)',
                       xaxis={
                             'showgrid': True,
                             'gridcolor': 'lightgray',
                             'linewidth': 1,
                             'linecolor': 'lightgray',
-                            'showline': True,  # 显示x轴线
-                            'mirror': True,  # 在对面也显示轴线
+                            'showline': True,
+                            'mirror': True,
                       },
                       yaxis={
                             'showgrid': True,
                             'gridcolor': 'lightgray',
                             'linewidth': 1,
                             'linecolor': 'lightgray',
-                            'showline': True,  # 显示y轴线
-                            'mirror': True,  # 在对面也显示轴线
+                            'showline': True,
+                            'mirror': True,
                       })
 
     fig.show()
-    # TODO: 添加baseline的线
 
 if __name__ == "__main__":
-    epoch_1 = {
-        "percentage": [0.05, 0.15, 0.3, 0.4, 0.6, 1],
-        "agr average": [2.16, 0.48, 1.62, 1.77, 2.99, 2.19]
+    data = {
+        "max length 512": {
+            "percentage": [0, 0.05, 0.15, 0.3, 0.4, 0.6, 0.8, 1],
+            "agr average": [0, 0.79, 2.078, 1.755, 1.247, 3.447, 2.967, 2.175]
+        },
+        "max length 1024": {
+            "percentage": [0, 0.05, 0.15, 0.3, 0.4, 0.6, 0.8, 1],
+            "agr average": [0, 1.266, 1.399, 1.723, 1.247, 2.581, 2.581, 1.291] # TODO: 0.4, 0.6
+        },
+        "max length 1536": {
+            "percentage": [0, 0.05, 0.15, 0.3, 0.4, 0.6, 0.8, 1],
+            "agr average": [0, 0.983, 1.622, 2.563, 1.453, 1.326, 1.225, 1.885]
+        },
+        "max length 2048": {
+            "percentage": [0, 0.05, 0.15, 0.3, 0.4, 0.6, 0.8, 1],
+            "agr average": [0, 0.365, 0.967, 2.231, 1.256, 1.616, 2.052, 1.529]
+        }
     }
-    epoch_2 = {
-        "percentage": [0.05, 0.15, 0.3, 0.4, 0.6, 1],
-        "agr average": [1.78, 0.52, 2.23, 2.21, 1.62, 1.53]
-    }
-    epoch_3 = {
-        "percentage": [0.05, 0.15, 0.3, 0.4, 0.6, 1],
-        "agr average": [1.39, 0.21, 1.60, 1.76, 1.31, 1.80]
-    }
 
-    df_epoch_1 = pd.DataFrame(epoch_1)
-    df_epoch_1['epoch'] = 'Epoch 1'
+    df_list = []
+    for length, values in data.items():
+        df_temp = pd.DataFrame(values)
+        df_temp['max length'] = length
+        df_list.append(df_temp)
 
-    df_epoch_2 = pd.DataFrame(epoch_2)
-    df_epoch_2['epoch'] = 'Epoch 2'
+    df = pd.concat(df_list, ignore_index=True)
 
-    df_epoch_3 = pd.DataFrame(epoch_3)
-    df_epoch_3['epoch'] = 'Epoch 3'
+    plot_metric_trend(df, 'percentage', 'agr average')
 
-    df_combined = pd.concat([df_epoch_1, df_epoch_2, df_epoch_3])
-
-    plot_metric_trend(df_combined, x='percentage', y='agr average')
